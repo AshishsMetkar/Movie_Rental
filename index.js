668afaf0bf9b135ac517fceb6f4490bbc26f801b
Ashish Metkar <ashish01@edulab.in>
2024-03-26 19:01:28 +0530

    deploy

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("express-async-errors")
 const config = require("config")
 const cors = require("cors")
 const app = express();
+const path = require('path')
 
 const mongoose = require('mongoose');
 
@@ -21,6 +22,21 @@ if(!config.get("password")){
   console.log("jwtPrivateKey is not set");
 }
 
+// line no 25 to 39 const buildPath = path.join(__dirname, "D:", "Movie_rental", "Movie-Rentals-Frontend", "build");
+const buildPath = "D:\\Movie_rental\\Movie-Rentals-Frontend\\build";
+app.use(express.static(buildPath));
+
+app.get("/", function(req, res){
+
+  res.sendFile(
+    path.join(buildPath, "index.html"),
+    function(err){
+      if (err){
+        res.status(500).send(err);
+      }
+    }
+  )
+})
 mongoose.connect('mongodb://127.0.0.1:27017/movie_rentals')
 .then(() => {
   console.log(`Connected to MongoDB server`);
