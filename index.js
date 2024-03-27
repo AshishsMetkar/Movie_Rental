5a2e16799d715bd9e8dc5cd3f50568cc64653680
Ashish Metkar <ashish01@edulab.in>
2024-03-27 09:38:49 +0530

    update Index.js file

--- index.js
+++ index.js
@@ -22,21 +22,21 @@ if(!config.get("password")){
   console.log("jwtPrivateKey is not set");
 }
 
-// line no 25 to 39 const buildPath = path.join(__dirname, "D:", "Movie_rental", "Movie-Rentals-Frontend", "build");
-const buildPath = "D:\\Movie_rental\\Movie-Rentals-Frontend\\build";
-app.use(express.static(buildPath));
+// // line no 25 to 39 const buildPath = path.join(__dirname, "D:", "Movie_rental", "Movie-Rentals-Frontend", "build");
+// const buildPath = "D:\\Movie_rental\\Movie-Rentals-Frontend\\build";
+// app.use(express.static(buildPath));
 
-app.get("/", function(req, res){
+// app.get("/", function(req, res){
 
-  res.sendFile(
-    path.join(buildPath, "index.html"),
-    function(err){
-      if (err){
-        res.status(500).send(err);
-      }
-    }
-  )
-})
+//   res.sendFile(
+//     path.join(buildPath, "index.html"),
+//     function(err){
+//       if (err){
+//         res.status(500).send(err);
+//       }
+//     }
+//   )
+// })
 mongoose.connect('mongodb://127.0.0.1:27017/movie_rentals')
 .then(() => {
   console.log(`Connected to MongoDB server`);
